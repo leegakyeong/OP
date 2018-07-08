4d4d799d7b24911cafe5f91fe8dc911867f8dd12
leegakyeong <alaska7132@naver.com>
2018-07-09 01:56:21 +0900

    also cleaned like master branch

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -1,20 +1,17 @@
 class ProjectsController < ApplicationController
-    # def index
-    #     puts '=================================='
-    #     # @projects = Project.all
-    #     render 'index'
-    #     # puts response.headers["content_type"]
-    # end
+    def index
+        render 'index'
+    end
 
     def search
         # 프로젝트 이름으로 검색
-        name = params[:name]
-        # @result = Project.find_by_name(name) 이렇게 하면 조건을 만족하는 첫 번째 것 하나밖에 가져올 수 없음
-        # @results = Project.where(name:name)
+        title = params[:title]
+        # @result = Project.find_by_title(title) 이렇게 하면 조건을 만족하는 첫 번째 것 하나밖에 가져올 수 없음
+        # @results = Project.where(title:title)
         @results = []
 
         Project.all.each do |p|
-            if p.name.include? name # 레일즈 3.1.0부터 include? 대신 in? 써도 된다더니 안 됨!!
+            if p.title.include? title # 레일즈 3.1.0부터 include? 대신 in? 써도 된다더니 안 됨!!
                 @results.push(p)
             end
         end
@@ -24,26 +21,5 @@ class ProjectsController < ApplicationController
             # 아무것도 안 보여주는 옵션도 생각해 보기
             # 이 페이지를 새로고침했을 때 메소드가 get이 되어서 오류가 나는 것도 고치기! 한 번 전달된 게 계속 남아 있거나 일치하는 게 없을 때와 같은 상황으로 남아 있으면 좋겠는데 
         end
-    end
-
-    # def new
-    # end
-
-    # def edit_view
-    # end
-
-    # def edit
-    # end
-
-    # def detail
-    # end
-
-    # def like
-    # end
-
-    # def request
-    # end
-
-    def accept
     end
 end
--- db/migrate/20180624112419_create_projects.rb
+++ db/migrate/20180624112419_create_projects.rb
@@ -3,8 +3,7 @@ class CreateProjects < ActiveRecord::Migration[5.1]
     create_table :projects do |t|
       # t.belongs_to :user
       t.integer :admin_id, foreign_key: true
-      t.string :name
-      t.text :description
+      t.string :title
 
       t.timestamps
     end
