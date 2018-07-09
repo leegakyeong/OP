fe59196cc6aec54f86d6ea12fb3d81f0263c06d1
ssm0318 <kleenex0318@gmail.com>
2018-07-09 18:32:41 +0900

    first commit

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -1,5 +1,36 @@
 class ProjectsController < ApplicationController
+    def new
+    end
+
+    def create
+        project = Project.new
+        project.admin_id = params[:admin_id]
+        project.title = params[:title]
+        project.maxMember = params[:maxMember]
+        project.skills = params[:skills]
+        project.description = params[:description]
+        project.isKorean = params[:isKorean]
+        project.isOnline = params[:isOnline]
+        project.tools = params[:tools]
+        project.files = params[:files]
+        project.tags = params[:tags]
+        project.save
+
+        redirect_to "/#{project.id}"
+    end
+    
     def index
         render 'index'
+    end
+
+    def show
+        @project = Project.find(params[:id])
+    end
+
+    def destroy
+        project = Project.find(params[:id])
+        project.destroy
+
+        redirect_to '/'
     end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,13 @@ Rails.application.routes.draw do
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   get '/' => 'projects#index'
+
+  get '/new' => 'projects#new'
+  post '/' => 'projects#create'
+
+  get '/:id' => 'projects#show'
+
+  delete '/:id' => 'projects#destroy'
+
+  #delete '/:id' => 'projects#destroy'
 end
