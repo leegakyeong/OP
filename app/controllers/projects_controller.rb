5ae8be13a9ab8c2a8f0df56629c69148df240cc2
leegakyeong <alaska7132@naver.com>
2018-07-09 20:14:47 +0900

    검색에 필터 적용(title, admin_id)

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -4,22 +4,23 @@ class ProjectsController < ApplicationController
     end
 
     def search
-        # 프로젝트 이름으로 검색
-        title = params[:title]
-        # @result = Project.find_by_title(title) 이렇게 하면 조건을 만족하는 첫 번째 것 하나밖에 가져올 수 없음
-        # @results = Project.where(title:title)
+        keyword = params[:keyword]
+        filter = params[:filter]
         @results = []
 
         Project.all.each do |p|
-            if p.title.include? title # 레일즈 3.1.0부터 include? 대신 in? 써도 된다더니 안 됨!!
-                @results.push(p)
+            case filter
+            when 'title'
+                if p.title.include? keyword
+                    @results.push(p)
+                end
+            when 'admin'
+                if p.admin.id == keyword.to_i
+                    @results.push(p)
+                end
+            else
+                @results = Project.all
             end
-        end
-
-        if @results.length == 0
-            @results = Project.all # 일치하는 검색 결과가 없을 때 그냥 모든 프로젝트를 다 보여 줌
-            # 아무것도 안 보여주는 옵션도 생각해 보기
-            # 이 페이지를 새로고침했을 때 메소드가 get이 되어서 오류가 나는 것도 고치기! 한 번 전달된 게 계속 남아 있거나 일치하는 게 없을 때와 같은 상황으로 남아 있으면 좋겠는데 
         end
     end
 end
