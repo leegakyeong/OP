fa4404afdca51fa7c39ac1123eabfc5637ef1c23
leegakyeong <alaska7132@naver.com>
2018-07-09 18:01:52 +0900

    added seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,12 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+User.create(email: 'a@a.com', password: 'aaaaaa')
+User.create(email: 'b@b.com', password: 'bbbbbb')
+User.create(email: 'c@c.com', password: 'cccccc')
+Project.create(title: 'apple', admin_id: 1)
+Project.create(title: 'banana', admin_id: 1)
+Project.create(title: 'carrot', admin_id: 2)
+Project.create(title: 'deer', admin_id: 2)
+Project.create(title: 'electricity', admin_id: 3)
