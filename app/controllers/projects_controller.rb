df2d62b8a27843b50d3c80710d96d337dd54f1c0
leegakyeong <alaska7132@naver.com>
2018-07-17 01:46:54 +0900

    search by title, admin name, maxMember, isKorean, and isOnline

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -28,53 +28,31 @@ class ProjectsController < ApplicationController
     end
 
     def search
-        # keyword = params[:keyword]
-        # maxMember = params[:maxMember]
-        # isKorean = params[:isKorean]
-        # isOnline = params[:isOnline]
-        # @results = []
+        keyword = params[:keyword]
+        maxMember = params[:maxMember]
+        isKorean = params[:isKorean]
+        isOnline = params[:isOnline]
+        @results = []
 
         Project.all.each do |p|
-            # keyword = params[:keyword]
-            # maxMember = params[:maxMember]
-            # isKorean = params[:isKorean]
-            # isOnline = params[:isOnline]
-            @results = []
-
-            def search_by_title_or_name(p)
-                keyword = params[:keyword]
-                maxMember = params[:maxMember]
-
-                if p.title.include? keyword or p.admin.name.include? keyword # 왜 or 말고 ||는 안 됨? 
-                    if p.maxMember <= maxMember.to_i
-                        return true
+            if p.title.include? keyword or p.admin.name.include? keyword # 왜 or 말고 ||는 안 됨? 
+                if p.maxMember <= maxMember.to_i
+                    if isKorean and isOnline
+                        if p.isKorean.to_s == isKorean and p.isOnline.to_s == isOnline
+                            @results.push(p)
+                        end
+                    elsif isKorean
+                        if p.isKorean.to_s == isKorean
+                            @results.push(p)
+                        end
+                    elsif isOnline
+                        if p.isOnline.to_s == isOnline
+                            @results.push(p)
+                        end
+                    else
+                        @results.push(p)
                     end
                 end
-            end
-
-            def filter_by_isKorean_and_isOnline(p)
-                isKorean = params[:isKorean]
-                isOnline = params[:isOnline]
-
-                if isKorean and isOnline
-                    if p.isKorean.to_s == isKorean and p.isOnline.to_s == isOnline
-                        return true
-                    end
-                elsif isKorean
-                    if p.isKorean.to_s == isKorean
-                        return true
-                    end
-                elsif isOnline
-                    if p.isOnline.to_s == isOnline
-                        return true
-                    end
-                else
-                    return true
-                end
-            end
-
-            if search_by_title_or_name(p) and filter_by_isKorean_and_isOnline(p)
-                @results.push(p)
             end
         end
     end
