cbeae6934ccbcd70599271529f314ca18279465d
goojinsun <gjinsun96@gmail.com>
2018-08-12 06:30:14 +0900

    sign up with name

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,12 +5,13 @@ class ApplicationController < ActionController::Base
  
   # protect_from_forgery with: :exception
    
-  # before_action :configure_permitted_parameters, if: :devise_controller?
+  before_action :configure_permitted_parameters, if: :devise_controller?
  
-  # protected
+  protected
  
-  # def configure_permitted_parameters
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:name, :email, :password, :password_confirmation])
   #   devise_parameter_sanitizer.for(:sign_up) { |u| u.permit(:username, :email, :password, :password_confirmation, :remember_me, :avatar, :avatar_cache) }
   #   devise_parameter_sanitizer.for(:account_update) { |u| u.permit(:username, :password, :password_confirmation, :current_password, :avatar) }
-  # end
+  end
 end
